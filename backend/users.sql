a5106e087b3faf25807574651ce49745fe1101dc
sermedina <sermedina@gmail.com>
2023-11-21 22:51:25 -0400

    users script

---
+++ backend/users.sql
@@ -0,0 +1,12 @@
+INSERT INTO public."Users"
+(email, "firstName", "lastName", "password", "role", "createdAt", "updatedAt")
+VALUES('secretaria@gmail.com', 'Maria', 'Perez', '123','SECRETARIA', '2023-11-19 21:08:42.847', '2023-11-19 21:08:42.847');
+
+
+INSERT INTO public."Users"
+(email, "firstName", "lastName", "password", "role", "createdAt", "updatedAt")
+VALUES('jefe@gmail.com', 'Juan', 'Perez', '123','JEFE', '2023-11-19 21:08:42.847', '2023-11-19 21:08:42.847');
+
+INSERT INTO public."Users"
+(email, "firstName", "lastName", "password", "role", "createdAt", "updatedAt")
+VALUES('encargadoalmacen@gmail.com', 'Luis', 'Perez', '123','ENCARGADO_DE_ALMACEN', '2023-11-19 21:08:42.847', '2023-11-19 21:08:42.847');
